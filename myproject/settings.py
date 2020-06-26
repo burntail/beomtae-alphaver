04f6b140b9d5406ae8cc9c6de4382bb8ef8a9b1c
beomtae <bumtae0209@naver.com>
2020-06-26 22:42:10 +0900

    second commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'al!$$wv$fq(s6^1+mz9ae0l7w3onwhv1x&28ecjoicqh+bt7q)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
